c0c3b7eb6a3cc6875ab719086ab484154b3e1b53
David Garcia <juandavidgarciaburbano@gmail.com>
2025-06-01 11:47:01 -0500

    Ultimos cambios

--- cargarFirestore.cjs
+++ cargarFirestore.cjs
@@ -2,20 +2,7 @@ const admin = require('firebase-admin');
 const fs = require('fs');
 
 // Reemplaza la ruta por la de tu archivo de credenciales
-const serviceAccount = {}
-//  {"type": import.meta.env.VITE_TYPE,
-//   "project_id": import.meta.env.VITE_PROJECT_ID,
-//   "private_key_id": import.meta.env.VITE_PRIVATE_KEY_ID,
-//   "private_key":  import.meta.env.VITE_PRIVATE_KEY,
-//   "client_email":  import.meta.env.VITE_CLIENT_EMAIL,
-//   "client_id": import.meta.env.VITE_CLIENT_ID,
-//   "auth_uri":  import.meta.env.VITE_AUTH_URI,
-//   "token_uri": import.meta.env.VITE_TOKEN_URI,
-//   "auth_provider_x509_cert_url":  import.meta.env.VITE_AUTH_PROVIDER_X509_CERT_URL ,
-//   "client_x509_cert_url":  import.meta.env.VITE_CLIENT_X509_CERT_URL,
-//   "universe_domain":  import.meta.env.VITE_UNIVERSE_DOMAIN 
-
-// }
+const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
